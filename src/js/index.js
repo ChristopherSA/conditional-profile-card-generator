015bc14ec04455b06b2905ebe9c3d6fa5743910f
ChristopherSA <christopher.sevilla@outlook.com>
2021-01-29 17:03:11 +0000

    terminado

--- src/js/index.js
+++ src/js/index.js
@@ -29,18 +29,63 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let name = "name";
+  if (variables.name !== null) {
+    name = variables.name;
+  }
+
+  let lastname = "lastname";
+  if (variables.lastname !== null) {
+    lastname = variables.lastname;
+  }
+
+  let role = "role";
+  if (variables.role !== null) {
+    role = variables.role;
+  }
+
+  let city = "city";
+  if (variables.city !== null) {
+    city = variables.city;
+  }
+
+  let country = "country";
+  if (variables.country !== null) {
+    country = variables.country;
+  }
+
+  let twitter = "twitter";
+  if (variables.twitter !== null) {
+    twitter = variables.twitter;
+  }
+
+  let github = "github";
+  if (variables.github !== null) {
+    github = variables.github;
+  }
+
+  let linkedin = "linkedin";
+  if (variables.linkedin !== null) {
+    linkedin = variables.linkedin;
+  }
+
+  let instagram = "instagram";
+  if (variables.instagram !== null) {
+    instagram = variables.instagram;
+  }
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${name},${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -61,7 +106,7 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: "christopherSA",
     linkedin: null,
     instagram: null,
     name: null,
